4913a402180e9ad9d86d3ae0b4f82d2efe4da1cd
Valberto Enoc <valbertoenoc@gmail.com>
2023-10-11 22:20:50 -0300

    iteration chapter

---
+++ go.mod
@@ -0,0 +1,3 @@
+module learnGoWithTests
+
+go 1.21.2
---
+++ test-hello/hello.go
@@ -0,0 +1,41 @@
+package hello
+
+import "fmt"
+
+const (
+	spanish    = "Spanish"
+	french     = "French"
+	portuguese = "Portuguese"
+
+	englishPrefix    = "Hello, "
+	spanishPrefix    = "Hola, "
+	frenchPrefix     = "Bonjour, "
+	portuguesePrefix = "Olá, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishPrefix
+	case french:
+		prefix = frenchPrefix
+	case portuguese:
+		prefix = portuguesePrefix
+	default:
+		prefix = englishPrefix
+	}
+
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
