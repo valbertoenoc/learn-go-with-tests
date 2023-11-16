f019cf35c41d1b60b107a2706ca0a6135d6fa7a4
Valberto Enoc <valbertoenoc@gmail.com>
2023-11-16 10:44:23 -0300

    reflection finished

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,20 +1,39 @@
 package reflection
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
 
 	switch val.Kind() {
 	case reflect.String:
 		fn(val.String())
 	case reflect.Struct:
 		for i := 0; i < val.NumField(); i++ {
-			walk(val.Field(i).Interface(), fn)
+			walkValue(val.Field(i))
 		}
-	case reflect.Slice:
+	case reflect.Slice, reflect.Array:
 		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walkValue(v)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
 		}
 	}
 }
